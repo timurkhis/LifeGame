1d999b8d3ec55309b23fade465a7a4a58002cd3f
Arsonist <gmoximko@icloud.com>
2017-03-22 12:56:55 +0700

    WSACleanUp calls only after all sockets destructors.

--- LifeGame/Messenger.cpp
+++ LifeGame/Messenger.cpp
@@ -77,6 +77,9 @@ namespace Messaging {
                 connection->Clear();
             }
         }
+        if (connections.size() == 0 && listener == nullptr) {
+            Close();
+        }
     }
 
     void Messenger::Write(const std::vector<TCPSocketPtr> &outWrite) {
@@ -111,9 +114,6 @@ namespace Messaging {
         auto iter = std::find(connections.begin(), connections.end(), connection);
         if (iter != connections.end()) {
             connections.erase(iter);
-        }
-        if (connections.size() == 0 && listener == nullptr) {
-            Close();
         }
     }
     
